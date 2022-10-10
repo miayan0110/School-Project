0ca4ad9914abd628580621be1ec33f155e140e03
miayan0110 <miayan0110@gmail.com>
2022-10-10 21:12:13 +0800

    Add skill effects
    2 skill effects: magic(water) and fire

--- Template/Assets/Scripts/Players/wizzard_m/PlayerWizzardM.cs
+++ Template/Assets/Scripts/Players/wizzard_m/PlayerWizzardM.cs
@@ -7,47 +7,54 @@ public class PlayerWizzardM : MonoBehaviour
     public float playerSpeed;
 
     SpriteRenderer playerSR;
-    Animator playerAni;
+    Animator playerAni, skillAni;
 
     void Start()
     {
         playerSR = this.GetComponent<SpriteRenderer>();
         playerAni = this.GetComponent<Animator>();
+        skillAni = GameObject.Find("SkillAnim").GetComponent<Animator>();
     }
 
     void Update()
     {
         MoveByWSAD();
+        Attack();
     }
 
     void MoveByWSAD()
     {
         if (Input.GetKey(KeyCode.W))
         {
-            //playerAni.SetBool("isRunning", true);
             playerAni.Play("run");
             this.gameObject.transform.position += new Vector3(0f, playerSpeed * Time.deltaTime, 0f);
         }
         else if (Input.GetKey(KeyCode.S))
         {
-            //playerAni.SetBool("isRunning", true);
             playerAni.Play("run");
             this.gameObject.transform.position -= new Vector3(0f, playerSpeed * Time.deltaTime, 0f);
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            //playerAni.SetBool("isRunning", true);
             playerAni.Play("run");
             playerSR.flipX = true;
             this.gameObject.transform.position -= new Vector3(playerSpeed * Time.deltaTime, 0f, 0f);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            //playerAni.SetBool("isRunning", true);
             playerAni.Play("run");
             playerSR.flipX = false;
             this.gameObject.transform.position += new Vector3(playerSpeed * Time.deltaTime, 0f, 0f);
+        }
+    }
+
+    void Attack()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerAni.Play("hit");
+            skillAni.Play("fire");
         }
     }
 }
