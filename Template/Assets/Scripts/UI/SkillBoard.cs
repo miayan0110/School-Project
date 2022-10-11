e01011cff5db8e24a7ff4123340e9c4791d60097
miayan0110 <miayan0110@gmail.com>
2022-10-11 10:09:21 +0800

    add new skill effect
    new skill effect: ice pillar

---
+++ Template/Assets/Scripts/UI/SkillBoard.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillBoard : MonoBehaviour
+{
+    public GameObject playerSkillOnEnemy;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void SkillOne()
+    {
+
+    }
+}
