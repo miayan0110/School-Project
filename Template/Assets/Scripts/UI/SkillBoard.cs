7374fd17c4cf9976fa5a9cf3eec56173f811e01b
miayan0110 <miayan0110@gmail.com>
2022-10-12 14:42:00 +0800

    button trigger skill effect

--- Template/Assets/Scripts/UI/SkillBoard.cs
+++ Template/Assets/Scripts/UI/SkillBoard.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SkillBoard : MonoBehaviour
 {
     public GameObject playerSkillOnEnemy;
+
+    Transform playerTR;
+    Image myIM;
+    Animator skillAnim;
+
     void Start()
     {
-        
+        playerTR = GameObject.Find("Player").GetComponent<Transform>();
+        myIM = this.gameObject.GetComponent<Image>();
+        skillAnim = playerSkillOnEnemy.GetComponent<Animator>();
     }
 
     void Update()
@@ -15,8 +23,28 @@ public class SkillBoard : MonoBehaviour
         
     }
 
-    public void SkillOne()
+    public void Skill()
     {
+        Instantiate(playerSkillOnEnemy, playerTR.position, Quaternion.identity);
+        StartCoroutine("Delete");
+        //StartCoroutine("PlaySkill");
+        /*
+        if (playerSkillOnEnemy.activeInHierarchy && skillAnim.GetCurrentAnimatorStateInfo(0).IsName("idle"))
+        {
+            playerSkillOnEnemy.SetActive(false);
+        }
+        */
+    }
 
+    IEnumerator PlaySkill()
+    {
+        yield return null;
+        skillAnim.Play(myIM.sprite.name);
+    }
+
+    IEnumerator Delete()
+    {
+        yield return new WaitForSeconds(2.0f);
+        Destroy(GameObject.Find("PlayerSkillOnEnemy(Clone)"));
     }
 }
